b3a0a58e40a1d809024b25ddbd395232345a1240
bo-yakitarako <bo-yakitarako@users.noreply.github.com>
2025-11-22 18:09:57 +0900

    ﾎﾟﾎﾟﾎﾟﾎﾟｰﾝ

--- src/components/slashCommands.ts
+++ src/components/slashCommands.ts
@@ -4,7 +4,7 @@ import {
   SlashCommandBuilder,
   VoiceChannel,
 } from 'discord.js';
-import { game } from '../WordWolf';
+import { game, WordWolf } from '../WordWolf';
 
 const flags = MessageFlags.Ephemeral;
 
@@ -20,47 +20,39 @@ const registration = {
       await wordWolf.join(interaction);
     },
   },
+  start: {
+    data: new SlashCommandBuilder()
+      .setName('start')
+      .setDescription('議論時間を指定してスタートしちゃおう')
+      .addIntegerOption((option) =>
+        option.setName('時間').setDescription('時間は秒数で指定してな？').setRequired(true),
+      ),
+    execute: async (interaction: ChatInputCommandInteraction, wordWolf: WordWolf) => {
+      const time = interaction.options.getInteger('時間', true);
+      await wordWolf.start(interaction, time);
+    },
+  },
   result: {
     data: new SlashCommandBuilder().setName('result').setDescription('投票が終わった時にやるやつ'),
-    execute: async (interaction: ChatInputCommandInteraction) => {
-      const wordWolf = game.get(interaction);
-      if (wordWolf === null) {
-        await interaction.reply({ content: 'おらんがな', flags });
-        return;
-      }
+    execute: async (interaction: ChatInputCommandInteraction, wordWolf: WordWolf) => {
       await wordWolf.goToResult(interaction);
     },
   },
   continue: {
     data: new SlashCommandBuilder().setName('continue').setDescription('続けてえよな？'),
-    execute: async (interaction: ChatInputCommandInteraction) => {
-      const wordWolf = game.get(interaction);
-      if (wordWolf === null) {
-        await interaction.reply({ content: 'おらんがな', flags });
-        return;
-      }
+    execute: async (interaction: ChatInputCommandInteraction, wordWolf: WordWolf) => {
       await wordWolf.continue(interaction);
     },
   },
   finish: {
     data: new SlashCommandBuilder().setName('finish').setDescription('終わりてえよな？'),
-    execute: async (interaction: ChatInputCommandInteraction) => {
-      const wordWolf = game.get(interaction);
-      if (wordWolf === null) {
-        await interaction.reply({ content: 'おらんがな', flags });
-        return;
-      }
+    execute: async (interaction: ChatInputCommandInteraction, wordWolf: WordWolf) => {
       await wordWolf.finish(interaction);
     },
   },
   bye: {
     data: new SlashCommandBuilder().setName('bye').setDescription('ｻﾖﾅﾗしちゃう'),
-    execute: async (interaction: ChatInputCommandInteraction) => {
-      const wordWolf = game.get(interaction);
-      if (wordWolf === null) {
-        await interaction.reply({ content: 'おらんがな', flags });
-        return;
-      }
+    execute: async (interaction: ChatInputCommandInteraction, wordWolf: WordWolf) => {
       await wordWolf.destroy();
       game.remove(interaction);
       await interaction.reply('ばいばーい');
@@ -77,5 +69,10 @@ export const slashCommandsInteraction = async (interaction: ChatInputCommandInte
     return;
   }
   const commandName = interaction.commandName as CommandName;
-  await registration[commandName].execute(interaction);
+  const wordWolf = game.get(interaction);
+  if (commandName !== 'wordwolf' && wordWolf === null) {
+    await interaction.reply({ content: 'おらんがな', flags });
+    return;
+  }
+  await registration[commandName].execute(interaction, wordWolf!);
 };
